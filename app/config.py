33b26b602b0c13132c5b7a93126e26c4e8b696a7
Patrikas <patrestak@gmail.com>
2026-07-04 21:05:35 +0300

    satellite test at 0

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ CAN_BITRATE = 250000
 SPEED_UNIT = "kph"
 SPEED_FULL_SCALE_KPH = 12.0
 SPEED_MAX_VALUE = 12.0 #dead code
-GPS_MIN_SATELLITES = 25
+GPS_MIN_SATELLITES = 0
 KW_FULL_SCALE_KW = 20.0
 
 # 0 = no bars, 1 = borto only, 2 = thruster only, 3 = both
